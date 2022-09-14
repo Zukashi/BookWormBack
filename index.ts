d1d4472a4944dd67f42231bb68abd229087d2bbb
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-14 08:28:11 +0200

    Added hashing passwords and comparing with hash, added Login router.

--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import mongoose from 'mongoose';
 import { registerRouter } from './routers/Register';
+import { loginRouter } from './routers/Login';
 
 const app = express();
 app.use(cors({
   origin: 'http://localhost:3000',
 }));
-
+(async function () {
+  await mongoose.connect('mongodb://localhost:27017/BookWorm');
+}());
 app.use(json());
 app.use('/', registerRouter);
+app.use('/', loginRouter);
 app.listen(3001, '0.0.0.0', () => {
   console.log('Listening on port http://localhost:3001');
 });
---
+++ routers/Login.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { User } from '../Schemas/User';
+
+const bcrypt = require('bcrypt');
+// eslint-disable-next-line import/prefer-default-export,no-undef
+export const loginRouter = Router();
+
+loginRouter.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.find({ username });
+  const hash = user[0].password;
+  const isSamePassword = await bcrypt.compare(password, hash);
+  if (isSamePassword) {
+    res.json(user[0]);
+  } else {
+    res.json({ error: 'error 404' });
+  }
+});
