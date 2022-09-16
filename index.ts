ddfe9d7a3442412681fbb7689019b604c4580955
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-16 19:52:40 +0200

    Fixed MongoDb ERRCONN bug added Author route
    # Conflicts:
    #	package.json

--- index.ts
+++ index.ts
@@ -4,17 +4,19 @@ import 'express-async-errors';
 import mongoose from 'mongoose';
 import { registerRouter } from './routers/Register';
 import { loginRouter } from './routers/Login';
+import { authorRouter } from './routers/Author';
 
 const app = express();
 app.use(cors({
   origin: 'http://localhost:3000',
 }));
 (async function () {
-  await mongoose.connect('mongodb://localhost:27017/BookWorm');
+  await mongoose.connect('mongodb://0.0.0.0:27017/BookWorm');
 }());
 app.use(json());
 app.use('/', registerRouter);
+app.use('/', authorRouter);
 app.use('/', loginRouter);
 app.listen(3001, '0.0.0.0', () => {
-  console.log('Listening on port http://localhost:3001');
+  console.log('Listening on port http://localhost:3000');
 });
