d9e27e9f8d06ce7e1c39acb9ba3b799f1d25d5fe
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-16 21:13:25 +0200

    Added data flow to URI path to get author by typing input

--- routers/Author.ts
+++ routers/Author.ts
@@ -4,9 +4,8 @@ export const authorRouter = Router();
 
 authorRouter.get('/author', async (req, res) => {
   const query = req.query.q as string;
-  const params = encodeURIComponent(query);
-  console.log(params);
-  const response = await fetch('https://openlibrary.org/search/authors.json?q=j%20k%20rowling');
+  const encodedQuery = encodeURIComponent(query);
+  const response = await fetch(`https://openlibrary.org/search/authors.json?q=${encodedQuery}`);
   const data = await response.json();
   console.log(data);
   res.json(data);
