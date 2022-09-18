e6c29ff74827b81445554a06a0af28bf10dd5f82
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-18 14:06:54 +0200

    Added search router

--- routers/Search.ts
+++ routers/Search.ts
@@ -3,10 +3,10 @@ import { Router } from 'express';
 export const searchRouter = Router();
 
 searchRouter.get('/search/:category/:key', async (req, res) => {
-  console.log(req.params);
   const { category, key } = req.params;
   const response = await fetch(`https://openlibrary.org/search.json?${category}=${key}`);
   const data = await response.json();
+  console.log(data.docs[0].language);
   console.log(data);
   res.json(data);
 });
