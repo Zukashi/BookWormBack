37aca6dbd3f2afddb5e4c299badec1dfe16dc9b1
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-20 17:19:06 +0200

    Finish Search Feature

--- routers/Search.ts
+++ routers/Search.ts
@@ -6,7 +6,6 @@ searchRouter.get('/search/:category/:key', async (req, res) => {
   const { category, key } = req.params;
   const response = await fetch(`https://openlibrary.org/search.json?${category}=${key}`);
   const data = await response.json();
-  console.log(data.docs[0].language);
-  console.log(data);
-  res.json(data);
+  console.log(category);
+  res.json(data.docs);
 });
