15dffe0393893ee7374e1869e1b5006f6575b2a2
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-23 19:22:14 +0200

    Fetching specified author in URI parameters from Open library API

--- routers/Author.ts
+++ routers/Author.ts
@@ -3,12 +3,16 @@ import { Router } from 'express';
 export const authorRouter = Router();
 
 authorRouter.get('/author', async (req, res) => {
-  // Searching for author based on Input
+  // Searching for author based on Search Bar Input
   const query = req.query.q as string;
   const encodedQuery = encodeURIComponent(query);
   const response = await fetch(`https://openlibrary.org/search/authors.json?q=${encodedQuery}`);
   const data = await response.json();
   res.json(data);
 }).get('/author/:authorId', async (req, res) => {
-
+  console.log(req.params.authorId);
+  const response = await fetch(`https://openlibrary.org/authors/${req.params.authorId}.json`);
+  const data = await response.json();
+  console.log(data);
+  res.end();
 });
