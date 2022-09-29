17cb4f9df95dcdb1dd9fd174d88c0aa71199c50f
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-29 10:45:29 +0200

    Add nodemailer

--- routers/Register.ts
+++ routers/Register.ts
@@ -9,27 +9,27 @@ export const registerRouter = Router();
 registerRouter.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
   const saltAmount = 10;
-  const mailTransporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'ziomski40@gmail.com',
-      pass: 'ZukashiHisaki2',
-    },
-  });
-  const details = {
-    from: 'ziomski40@gmail.com',
-    to: 'bartek.kaszowski@spoko.pl',
-    subject: 'testing our nodemailer',
-    text: 'testing',
-  };
-
-  mailTransporter.sendMail(details, (err) => {
-    if (err) {
-      console.log('error of ', err);
-    } else {
-      console.log('email has been sent');
-    }
-  });
+  // const mailTransporter = nodemailer.createTransport({
+  //   service: 'gmail',
+  //   auth: {
+  //     user: 'ziomski40@gmail.com',
+  //     pass: 'ZukashiHisaki2',
+  //   },
+  // });
+  // const details = {
+  //   from: 'ziomski40@gmail.com',
+  //   to: 'bartek.kaszowski@spoko.pl',
+  //   subject: 'testing our nodemailer',
+  //   text: 'testing',
+  // };
+  //
+  // mailTransporter.sendMail(details, (err) => {
+  //   if (err) {
+  //     console.log('error of ', err);
+  //   } else {
+  //     console.log('email has been sent');
+  //   }
+  // });
   bcrypt.hash(password, saltAmount, async (err:string, hash:string) => {
     const user = new User({
       username,
