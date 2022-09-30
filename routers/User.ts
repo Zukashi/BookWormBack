9c342ced53f4449d2835e352765e9cf1174f7449
Bartek Kaszowski <bartek.kaszowski@spoko.pl>
2022-09-30 11:20:32 +0200

    Added password validation and password update

--- routers/User.ts
+++ routers/User.ts
@@ -1,7 +1,23 @@
 import { Router } from 'express';
+import { User } from '../Schemas/User';
+
+const bcrypt = require('bcrypt');
 
 export const userRouter = Router();
 
-userRouter.put('/:userId', (req, res) => {
-  console.log(req.body);
-});
+userRouter
+  .put('/password', async (req, res) => {
+    const user = await User.findById(`${req.body.id}`);
+    console.log(req.body.currentPassword);
+    const isSamePassword = await bcrypt.compare(req.body.currentPassword, user.password);
+    if (isSamePassword) {
+      if (req.body.newPassword === req.body.verifyPassword) {
+        bcrypt.hash(req.body.verifyPassword, 10, async (err:string, hash:string) => {
+          user.password = hash;
+          await user.save();
+        });
+      }
+    } else {
+      res.json('Passwords dont match');
+    }
+  });
