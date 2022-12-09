7773290fb66833a548bbde54a45d231f09a69e86
Bartek <bartek.kaszowski@spoko.pl>
2022-12-09 18:41:10 +0100

    Added book Schema

---
+++ Schemas/Book.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  isbn: {
+    type: String,
+    required: true,
+    min: 13,
+    max: 13,
+  },
+});
+export const Book = mongoose.model('Book', bookSchema);
