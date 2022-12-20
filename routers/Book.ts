c14690bdddaecd3b233b7ed1781a2dd80c069f78
Bartek <bartek.kaszowski@spoko.pl>
2022-12-20 13:33:01 +0100

    addbook adds personal name of author instaed of input from form sent by post method in react

--- routers/Book.ts
+++ routers/Book.ts
@@ -12,25 +12,24 @@ bookRouter.get('/books', async (req, res) => {
   //   return data;
   // });
   // const values = await Promise.all(result);
-  console.log(books);
   res.json(books);
 }).post('/book', async (req, res) => {
   const { isbn, title, author } = req.body;
 }).post('/addBook', async (req, res) => {
   const { title, author, isbn } = req.body;
-  console.log(req.body);
   const response = await fetch(`https://openlibrary.org/isbn/${isbn}.json`);
   const data = await response.json();
   const response2 = await fetch(`https://openlibrary.org${data.works[0].key}.json`);
   const data2 = await response2.json();
-  console.log(data);
+  const response3 = await fetch(`http://localhost:3001/author${data.authors[0].key}`);
+  const data3 = await response3.json();
   const book = new Book({
 
     title: data.title,
     description: data2.description,
     subjects: data2.subjects,
     subject_people: data2.subject_people,
-    author,
+    author: data3.personal_name,
     isbn,
     ...data,
     authors: data.authors,
