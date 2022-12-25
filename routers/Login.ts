b19e412263450eb4d3ce892b981e212604eb88db
Bartek <bartek.kaszowski@spoko.pl>
2022-12-25 14:05:00 +0100

    Revert "Added POST refresh token logic"
    This reverts commit 1ce7c918ba2be88fcb2dca2b62ee4b1d25f70d11.

--- routers/Login.ts
+++ routers/Login.ts
@@ -5,24 +5,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 // eslint-disable-next-line import/prefer-default-export,no-undef
 export const loginRouter = Router();
-let refreshTokens:any[] = [];
-loginRouter.delete('/logout', (req, res) => {
-  refreshTokens = refreshTokens.filter((token) => token !== req.cookies.token);
-});
-loginRouter.post('/token', (req, res) => {
-  const { refreshToken } = req.cookies;
-  if (refreshToken === null) return res.sendStatus(401);
-  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
-  res.clearCookie('token', { httpOnly: true, secure: true });
-  jwt.verify(refreshToken, process.env.REFRESH, (err:any, user:any) => {
-    if (err) return res.sendStatus(403);
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' });
-    res.cookie('token', accessToken, {
-      httpOnly: true,
-      secure: true,
-    });
-  });
-});
+
 loginRouter.post('/login', async (req, res) => {
   const { username, password } = req.body;
   const user = await User.find({ username });
@@ -31,10 +14,10 @@ loginRouter.post('/login', async (req, res) => {
   const isSamePassword = await bcrypt.compare(password, hash);
   if (isSamePassword) {
     const userJWT = { name: user[0]._id };
-    const accessToken = jwt.sign(userJWT, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' });
-    const refreshToken = jwt.sgin(userJWT, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1h' });
-    refreshTokens.push(refreshToken);
-    res.json({ user: user[0], accessToken, refreshToken }).cookie('token', accessToken, { httpOnly: true, secure: true });
+    const accessToken = jwt.sign(userJWT, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+    res.json({ user: user[0], accessToken }).cookie('token', accessToken, {
+      httpOnly: true,
+    });
   } else {
     res.json({ error: 'error 404' });
   }
