82ebe06e085d322dc52b9b4587d818ef3788aca8
Bartek <bartek.kaszowski@spoko.pl>
2022-12-27 20:49:44 +0100

    Added tokens to handle logging in

--- routers/Login.ts
+++ routers/Login.ts
@@ -5,21 +5,27 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 // eslint-disable-next-line import/prefer-default-export,no-undef
 export const loginRouter = Router();
-const refreshTokens:string[] = [];
-loginRouter.post('/token', async (req, res) => {
+const refreshTokens:string[] = ['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTViMzljOTY2YTU1YzcyYjM0MTJmMiIsImlhdCI6MTY3MjE2NDE5MSwiZXhwIjoxNjcyNzY4OTkxfQ.ssWlXxRDBKLgdCPfShqubWWvcKvWdvqaEx2rqCXkec0'];
+loginRouter.post('/auth/refreshToken', async (req, res) => {
   const { refreshToken } = req.cookies;
-  if (refreshToken === null) return res.sendStatus(401);
+
+  if (refreshToken === null) return res.sendStatus(401).redirect('/');
   if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err:any, user:any) => {
     if (err) return res.sendStatus(403);
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+    const accessToken = jwt.sign({ id: user.id }, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: '15m',
+    });
     const accessCookieExpiryDate = new Date(Date.now() + 60 * 15 * 1000);
+
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
       sameSite: 'none',
       secure: true,
       expires: accessCookieExpiryDate,
-    });
+    }).status(201).json({ user: user.id, token: accessToken });
   });
 });
 
@@ -30,12 +36,17 @@ loginRouter.post('/login', async (req, res) => {
   const hash = user[0].password;
   const isSamePassword = await bcrypt.compare(password, hash);
   if (isSamePassword) {
-    const userJWT = { name: user[0]._id };
-    const accessToken = jwt.sign(userJWT, process.env.ACCESS_TOKEN_SECRET);
-    const refreshToken = jwt.sign(userJWT, process.env.REFRESH_TOKEN_SECRET);
+    const userJWT = { id: user[0]._id };
+    const accessToken = jwt.sign(userJWT, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: '15m',
+    });
+    const refreshToken = jwt.sign(userJWT, process.env.REFRESH_TOKEN_SECRET, {
+      expiresIn: '7d',
+    });
     refreshTokens.push(refreshToken);
     const accessCookieExpiryDate = new Date(Date.now() + 60 * 15 * 1000);
     const refreshCookieExpiryDate = new Date(Date.now() + 60 * 60 * 1000 * 24 * 7);
+    console.log(accessCookieExpiryDate.getTime(), refreshCookieExpiryDate.getTime());
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
       sameSite: 'none',
