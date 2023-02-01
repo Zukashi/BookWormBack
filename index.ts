f65a6c8371540e965435f317992dbd50d62d14b1
Bartek <bartek.kaszowski@spoko.pl>
2023-02-01 23:58:08 +0100

    0.0.0.0 to localhost

--- index.ts
+++ index.ts
@@ -36,6 +36,6 @@ app.use('/', searchRouter);
 app.use('/user', userRouter);
 app.use('', bookRouter);
 app.use(handleError);
-app.listen(3001, '0.0.0.0', () => {
+app.listen(3001, 'localhost', () => {
   console.log('Listening on port http://localhost:3000');
 });
