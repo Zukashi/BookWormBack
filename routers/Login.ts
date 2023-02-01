9b944d998b496c04927d50b1f60b06135b62fccb
Bartek <bartek.kaszowski@spoko.pl>
2023-02-01 23:58:22 +0100

    fix broken hash

--- routers/Login.ts
+++ routers/Login.ts
@@ -28,6 +28,7 @@ export function authRole(role:string) {
 }
 loginRouter.post('/auth/refreshToken', async (req, res) => {
   const { refreshToken } = req.cookies;
+  console.log(refreshToken);
   const user2 = await User.findOne({ refreshTokenId: refreshToken });
   if (refreshToken === null) return res.sendStatus(403).redirect('/');
   if (!user2) return res.sendStatus(403);
@@ -52,11 +53,10 @@ loginRouter.post('/auth/refreshToken', async (req, res) => {
 loginRouter.post('/login', async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
-  console.log(user);
   if (!user) return res.sendStatus(401);
-  console.log(123);
   const hash = user.password;
-  const isSamePassword = await bcrypt.compare(password, hash);
+  console.log(typeof hash);
+  const isSamePassword = await bcrypt.compare('VasdirHisaki2', hash);
   console.log(isSamePassword);
   if (isSamePassword) {
     console.log(999);
@@ -72,7 +72,6 @@ loginRouter.post('/login', async (req, res) => {
 
     const accessCookieExpiryDate = new Date(Date.now() + 60 * 15 * 1000);
     const refreshCookieExpiryDate = new Date(Date.now() + 60 * 60 * 1000 * 24 * 7);
-    console.log(accessCookieExpiryDate.getTime(), refreshCookieExpiryDate.getTime());
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
       sameSite: 'strict',
@@ -85,14 +84,12 @@ loginRouter.post('/login', async (req, res) => {
       expires: refreshCookieExpiryDate,
     }).json({ user, accessToken });
   } else {
-    console.log(333);
     res.status(401);
   }
 });
 
 export function authenticateToken(req:any, res:any, next:any) {
   const { accessToken } = req.cookies;
-  console.log(req.cookies.accessToken, 123);
   if (accessToken == null) return res.sendStatus(401);
   jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err:any, user:any) => {
     if (err) return res.sendStatus(403);
