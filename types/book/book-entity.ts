fae8a3f89ae3cd34f9ba8feaecd52f5ef39a429b
Bartek <bartek.kaszowski@spoko.pl>
2023-02-02 17:20:42 +0100

    add newBookEntity interface and BookEntity interface

---
+++ types/book/book-entity.ts
@@ -0,0 +1,48 @@
+import { Types } from 'mongoose';
+import { UserEntity } from '../users/user.entity';
+
+export interface NewBookEntity {
+    isbn:string,
+    author?:string,
+    title?:string
+    _id?:Types.ObjectId,
+}
+
+export interface BookEntity extends NewBookEntity {
+    author?:string,
+    isbn:string,
+    publish_date?:string,
+    publishers?:string[],
+    languages?:{key:string, _id:string}[],
+    authors?:{key:string, _id:string}[],
+    subjects?:string[],
+    subject_people?:string[],
+    description?:string,
+    ratingTypeAmount?:number[],
+    rating?:number,
+    amountOfRates?:number,
+    sumOfRates?:number,
+    reviews?: {
+        readonly _id:string,
+        readonly user:UserEntity,
+        description?:string,
+        rating:number,
+        status:string,
+        date:Date,
+        likes:{
+            usersThatLiked:{
+                user:UserEntity,
+                _id:string,
+            }[],
+            amount:number,
+        },
+        spoilers:boolean,
+        comments:{
+            user:UserEntity,
+            commentMsg:string,
+            date:Date,
+            _id:string,
+
+        }
+    }[]
+}
