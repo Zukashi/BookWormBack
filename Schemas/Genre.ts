7dcfbb5525ada9e12eec2c81405d7099d2d7c05a
Bartek <bartek.kaszowski@spoko.pl>
2023-02-05 23:44:35 +0100

    fis schema Genre

--- Schemas/Genre.ts
+++ Schemas/Genre.ts
@@ -4,5 +4,7 @@ import { BookEntity } from '../types';
 
 export const genreSchema = new mongoose.Schema<any>({
   genres: [String],
+  years: [String],
+  authors: [String],
 });
 export const Genre = mongoose.model<any>('Genre', genreSchema);
