746b8d5203df5baf254ecb6398c1de110fcd1365
Bartek <bartek.kaszowski@spoko.pl>
2023-02-08 23:44:22 +0100

    test filtering of user

--- tests/user.record.test.ts
+++ tests/user.record.test.ts
@@ -1,9 +1,13 @@
 import mongoose, { HydratedDocument } from 'mongoose';
+import express from 'express';
 import { UserRecord } from '../records/user.record';
 import { server } from '../index';
 import { UserEntity } from '../types';
+import { filterUsersByValue } from '../functions/users/getFilteredUsersByValue';
+import { User } from '../Schemas/User';
 
 let userNew:UserRecord;
+
 beforeAll(() => {
   userNew = new UserRecord({
     id: undefined,
@@ -34,4 +38,14 @@ test('is  instance of userRecord', async () => {
 test('user should be exist', async () => {
   const user = await UserRecord.getUser('63dac1e49dcd4c2de18bdf5d');
   expect(user).toBeDefined();
+});
+
+test('test if filtering users is correct', async () => {
+  const users:UserRecord[] = await UserRecord.getAllUsers();
+  const user = await User.findOne({ username: 'Zukashi_' }).lean();
+
+  const filteredUsers = filterUsersByValue(users, 'Zukashi');
+
+  expect(filteredUsers).toHaveLength(1);
+  expect(new UserRecord(user)).toStrictEqual(filteredUsers[0]);
 });
