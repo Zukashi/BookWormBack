1c6f3c5a0e33026ec18b684146535dbbb49c5caf
Bartek <bartek.kaszowski@spoko.pl>
2023-03-22 22:20:39 +0100

    get searched books by either title or author

--- routers/Search.ts
+++ routers/Search.ts
@@ -1,17 +1,11 @@
 import { Router } from 'express';
+import { Book } from '../Schemas/Book';
 
 export const searchRouter = Router();
 
-searchRouter.get('/search/:category/:key', async (req, res) => {
-  const { category, key } = req.params;
-  const response = await fetch(`https://openlibrary.org/search.json?${category}=${key}`);
-  const data = await response.json();
-  console.log(data.docs);
-  res.json(data.docs);
-}).get('/works/:bookId', async (req, res) => {
-  console.log(req.params.bookId);
-  const response = await fetch(`https://openlibrary.org/works/${req.params.bookId}.json`);
-  const data = await response.json();
-  console.log(data);
-  res.json(data);
+searchRouter.get('/search', async (req, res) => {
+  const q = req.query.q as string;
+  const regex = new RegExp(q, 'i');
+  const books = await Book.find({ [req.query.category as string]: regex });
+  res.json(books);
 });
