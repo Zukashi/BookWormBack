f05cc595ba971e41a1e39f2995657f95c758aa51
Bartek <bartek.kaszowski@spoko.pl>
2023-03-22 22:19:45 +0100

    repeatPassword joi schema for user register validation

--- validators/validateRegister.ts
+++ validators/validateRegister.ts
@@ -6,12 +6,12 @@ export const validateRegister = async (req:Request, res:Response, next:NextFunct
     username: Joi.string().min(6).max(18).required(),
     email: Joi.string().required().email(),
     password: Joi.string().min(8).max(24),
+    repeatPassword: Joi.string().min(8).max(24),
   });
   try {
     await schema.validateAsync(req.body);
     next();
   } catch (e) {
-    console.log(e);
     res.status(400).json({ status: 'false', message: e.details[0].message, type: e.details[0].context.label });
   }
 };
