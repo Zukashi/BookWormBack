6ce89bfab100248198abdaf36a0e7e825645030b
Bartek <bartek.kaszowski@spoko.pl>
2023-03-24 23:08:34 +0100

    search open library api removing duplicative titles and returning only those books with authors

--- routers/Search.ts
+++ routers/Search.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import axios from 'axios';
 import { Book } from '../Schemas/Book';
 
 export const searchRouter = Router();
@@ -7,4 +8,31 @@ searchRouter.get('/search', async (req, res) => {
   const regex = new RegExp(req.query.q as string, 'i');
   const books = await Book.find({ [req.query.category as string]: regex });
   res.json(books);
+}).get('/searchOL', async (req, res) => {
+  const { q, page } = req.query;
+  try {
+    const response = await axios.get('https://openlibrary.org/search.json', {
+      params: { q, page },
+    });
+    console.log(1);
+    const seen = new Set();
+    const uniqueTitles = response.data.docs.filter((item:any) => {
+      const duplicate = seen.has(item.title);
+      seen.add(item.title);
+      return !duplicate;
+    });
+    const hasAuthor = response.data.docs.filter((item:any) => {
+      if (item.author_name) {
+        return true;
+      }
+      return false;
+    });
+    response.data.docs = [
+      ...uniqueTitles,
+      ...hasAuthor,
+    ];
+    console.log(response.data.docs);
+    res.json(response.data);
+  } catch (e) {
+  }
 });
