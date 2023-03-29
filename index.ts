a0a76f34bbfef214f9de831c8abf8cd876bc21b3
Bartek <bartek.kaszowski@spoko.pl>
2023-03-29 22:38:13 +0200

    back to default

--- index.ts
+++ index.ts
@@ -21,24 +21,10 @@ dotenv.config();
 }());
 const cookieParser = require('cookie-parser');
 
+const cors = require('cors');
+
 export const app = express();
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://book-worm-kjh8.onrender.com');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors(corsOptions));
 
 app.use(cookieParser());
 app.use(json());
