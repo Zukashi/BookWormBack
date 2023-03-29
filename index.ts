da7f6a3ff861b2fda86b886d7c0af0ad1cb3faa3
Bartek <bartek.kaszowski@spoko.pl>
2023-03-29 22:15:56 +0200

    trying different cors options

--- index.ts
+++ index.ts
@@ -21,14 +21,24 @@ dotenv.config();
 }());
 const cookieParser = require('cookie-parser');
 
-const cors = require('cors');
-
 export const app = express();
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  // Pass to next layer of middleware
   next();
 });
-app.use(cors(corsOptions));
 
 app.use(cookieParser());
 app.use(json());
