36f11e8cc7dceb14a579206d21ddc488d3dc2535
Bartek <bartek.kaszowski@spoko.pl>
2023-04-07 17:36:15 +0200

    update allowed origins

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -1,1 +1,1 @@
-export const allowedOrigins = ['https://book-worm-kjh8.onrender.com', 'http://localhost:3000'];
+export const allowedOrigins = ['https://book-worm-kjh8.onrender.com', 'http://localhost:3000', 'https://delightful-buttercream-4ec635.netlify.app'];
